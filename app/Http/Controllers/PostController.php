42fa00e0b031babffb20dc2be1676d210bb5d1e6
Titipat <ttpsvb@gmail.com>
2017-09-25 15:37:36 +0700

    Create post model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,8 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-          'title' => 'Post'
+          'title' => 'Post',
+          'posts' => []
         ]);
     }
 }
