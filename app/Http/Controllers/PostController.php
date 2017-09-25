bf6e91752045748472bf92fac2f57578411408f2
Titipat <ttpsvb@gmail.com>
2017-09-25 15:49:14 +0700

    Implement posts view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::all();
+        // return $posts;
         return view('posts.index', [
           'title' => 'Post',
-          'posts' => []
+          'posts' => $posts
         ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,5 +4,8 @@
 <div class="blog-header">
   <h1 class="blog-title">Post</h1>
   <p class="lead blog-description">All posts should belong here.</p>
+  @foreach ($posts as $post)
+    <p>This is post id: {{ $post->id }}, {{ $post->title }}</p>
+  @endforeach
 </div>
 @endsection
